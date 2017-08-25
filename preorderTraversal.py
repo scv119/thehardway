42a1225fbbfc021abe1149002eef1a123c8ee71a
fanmengying <fanme@oregonstate.edu>
2017-08-25 15:59:14 -0700

    add threaded binary tree method

--- preorderTraversal.py
+++ preorderTraversal.py
@@ -6,16 +6,12 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
-<<<<<<< HEAD
-
-=======
->>>>>>> b45e09dc636b1fae85f50037f2377fc937c83bdb
 class Solution(object):
     def preorderTraversal(self, root):
         """
         :rtype: List[int]
         """
-        # time: O(n), space: O(1) if not consider return list
+        # time: O(n), space: O(h), n is the number of nodes, h is the height of this tree
         if root == None:
             return []
         return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
@@ -34,4 +30,29 @@ class Solution(object):
                 stack.append(temp.right)
             if temp.left != None:
                 stack.append(temp.left)
+        return result_list
+
+    def preorderTraversal3(self, root):
+        if root == None:
+            return []
+        result_list = []
+        parent = root
+        while parent != None:
+            if parent.left != None:
+                pre = parent.left
+                # find most right child node of parent's left node and make it point to parent node
+                # if it already points to parent node, it means we have already finished left part, then release the relationship and move on to right node of parent
+                while pre.right != None and pre.right != parent:
+                    pre = pre.right
+                if pre.right == None:
+                    result_list.append(parent.val)
+                    pre.right = parent
+                    parent = parent.left
+                else:
+                    pre.right = None
+                    parent = parent.right
+            # parent has no left node, then append parent value and moves on to right node of parent
+            else:
+                result_list.append(parent.val)
+                parent = parent.right
         return result_list
