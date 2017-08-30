7b95ef71706f3495ab3f6301167685bbdd7b3c1f
fanmengying <fanme@oregonstate.edu>
2017-08-30 01:16:25 -0700

    add another method

--- kthSmallest.py
+++ kthSmallest.py
@@ -1,3 +1,4 @@
+# leetcode: https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
@@ -21,5 +22,16 @@ class Solution(object):
         return self.inorder(root.left) + [root.val] + self.inorder(root.right)
 
     def kthSmallest(self, root, k):
+        n = self.countNode(root.left)
+        if k <= n:
+            return self.kthSmallest(root.left, k)
+        if k == n + 1:
+            return root.val
+        if k > n + 1:
+            return self.kthSmallest(root.right, k - n - 1)
+    def countNode(self, root):
+        if root == None:
+            return 0
+        return 1 + self.countNode(root.left) + self.countNode(root.right)
 
 
